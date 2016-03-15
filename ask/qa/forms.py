94be334c5624efce71d229bef2798ea62f4f6956
Kantair <tkachevanse@gmail.com>
2016-03-15 08:24:48 +0300

    1

---
+++ ask/qa/forms.py
@@ -0,0 +1,82 @@
+from django import forms
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from qa.models import Question, Answer
+
+class AskForm(forms.Form) :
+	title = forms.CharField(max_length=255)
+	text = forms.CharField(widget=forms.Textarea)
+	author = 1
+	
+	def clean_title(self) :
+		title = self.cleaned_data['title']
+		return title
+
+	def clean_text(self) :
+		text = self.cleaned_data['text']
+		return text
+		
+	def save(self) :
+		quest = Question.objects.create(title=self.cleaned_data['title'], text=self.cleaned_data['text'], author=self.author)
+		#quest.save()
+		return quest
+	
+class AnswerForm(forms.Form) :
+	text = forms.CharField(widget=forms.Textarea)
+	question = forms.IntegerField(widget=forms.HiddenInput)
+	author = 1
+	
+	def clean_text(self) :
+		text = self.cleaned_data['text']
+		return text
+		
+	def clean_question(self) :
+		try :
+			quest_id = int(self.cleaned_data['question'])
+		except ValueError :
+			raise forms.ValidationError('fail input')
+		return quest_id
+	
+	def save(self) :
+		answer = Answer.objects.create(text=self.cleaned_data['text'], question=self.cleaned_data['question'], author=self.author)
+		#answer.save()
+		return answer
+		
+class SignupForm(forms.Form) :
+	username = forms.CharField(max_length=50)
+	email = forms.EmailField(max_length=100)
+	password = forms.CharField(widget=forms.PasswordInput)
+	
+	def clean_username(self) :
+		username = self.cleaned_data['username']
+		return username
+		
+	def clean_email(self) :
+		email = self.cleaned_data['email']
+		return email
+		
+	def clean_password(self) :
+		password = self.cleaned_data['password']
+		
+	def save(self) :
+		user = User.objects.create_user(self.cleaned_data['username'], self.cleaned_data['email'], self.cleaned_data['password'])
+		return user
+	
+	def loginUser(self) :
+		user = authenticate(username=self.cleaned_data['username'], password=self.cleaned_data['password'])
+		return user
+		
+class LoginForm(forms.Form) :
+	username = forms.CharField(max_length=50)
+	password = forms.CharField(widget=forms.PasswordInput)
+	
+	def clean_username(self) :
+		username = self.cleaned_data['username']
+		return username
+		
+	def clean_password(self) :
+		password = self.cleaned_data['password']
+		
+	def loginUser(self) :
+		user = authenticate(username=self.cleaned_data['username'], password=self.cleaned_data['password'])
+		return user
---
+++ ask/qa/views.py
@@ -0,0 +1,138 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
+from qa.models import Question, Answer
+from django.contrib.auth import authenticate, login
+from qa.forms import AskForm, AnswerForm, SignupForm
+
+def test(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+def index(request) :
+	pageLimit = 10
+	#Entry.objects.order_by(Coalesce('summary', 'headline').desc()) #asc()
+	qwests = Question.objects.all().order_by('-id')
+	
+	from django.core.paginator import Paginator
+	
+	page = request.GET.get('page') or 1
+	try :
+		page = int(page)
+	except ValueError :
+		page = 1
+	paginator = Paginator(qwests, pageLimit)
+	paginator.baseurl = '/?page='
+	try :
+		page = paginator.page(page)
+	except EmptyPage :
+		page = paginator.page(paginator.num_pages)
+	return render(request, 'questionList.html', {
+		'title' : 'qwests and answers',
+		'list' : page.object_list,
+		'paginator' : paginator, 
+		'page' : page,
+	})
+	
+
+def popular(request) :
+	pageLimit = 10
+	#Entry.objects.order_by(Coalesce('summary', 'headline').desc()) #asc()
+	qwests = Question.objects.all().order_by('-likes')
+	
+	from django.core.paginator import Paginator
+	
+	page = request.GET.get('page') or 1
+	try :
+		page = int(page)
+	except ValueError :
+		page = 1
+	paginator = Paginator(qwests, pageLimit)
+	paginator.baseurl = '/?page='
+	try :
+		page = paginator.page(page)
+	except EmptyPage :
+		page = paginator.page(paginator.num_pages)
+	return render(request, 'questionList.html', {
+		'title' : 'popular quests',
+		'list' : page.object_list,
+		'paginator' : paginator, 
+		'page' : page,
+	})
+
+def question(request, quest_id) :
+	try :
+		quest = Question.objects.get(id = quest_id)
+	except Question.DoesNotExist :
+		raise Http404
+	answers = Answer.objects.all().filter(question = quest)
+	
+	title = 'qwest ' + quest_id
+	
+	user = request.user
+	
+	if user.is_authenticated() :
+		form = AnswerForm(initial={'question' : quest_id})
+	
+	return render(request, 'question.html', {
+		'title' : title,
+		'question' : quest,
+		'list' : answers,
+		'form' : form,
+	})
+
+def ask(request) :
+	user = request.user
+	if not user.is_authenticated():
+		raise Http404
+	if request.method == "POST" :
+		#print("POST!!!!!!!!!!!!!!!!!!!!!!!!!")
+		form = AskForm(request.POST)
+		if form.is_valid():
+			#print("FORM IS VALID!!!!!!!!!!!!")
+			form.author = user
+			quest = form.save()
+			#print("QUEST IS CREATE!!!!!!!!!!")
+			url = quest.get_absolute_url()
+			#print("URL = " + url +"!!!!!!!!!")
+			return HttpResponseRedirect(url)
+	else :
+		form = AskForm()
+	return render(request, 'ask_add.html', {
+		'form' : form
+	})
+	
+def answer(request) :
+	if request.method == "POST" :
+		form = AnswerForm(request.POST)
+		if form.is_valid():
+			answer = form.save()
+			url = '/question/' + form.question
+			return HttpResponseRedirect(url)
+			
+def signup(request) :
+	if request.method == "POST" :
+		form = SignupForm(request.POST)
+		if form.is_valid() :
+			form.save()
+			#user = form.loginUser()
+			user = authenticate(username=request.POST['username'], password=request.POST['password'])
+			login(request, user)
+			return HttpResponseRedirect("/")
+	else :
+		form = SignupForm()
+	return render(request, 'ask_add.html', {
+		'form' : form,
+	})
+	
+def login(request) :
+	if request.method == "POST" :
+		form = LoginForm(request.POST)
+		if form.is_valid() :
+			user = form.loginUser()
+			if user is not None :
+				if user.is_active :
+					login(request, user)
+					return HttpResponseRedirect("/")
+	else :
+		form = LoginForm()
+	return render(request)
